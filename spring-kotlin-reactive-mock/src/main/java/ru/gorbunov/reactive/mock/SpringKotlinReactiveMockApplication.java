a904232779ed2e86a60da429afc8969333422cb7
gorbunov-ia <Grbnv.vn@gmail.com>
2021-02-26 23:45:19 +0300

    Mock service project was added

---
+++ spring-kotlin-reactive-mock/src/main/java/ru/gorbunov/reactive/mock/SpringKotlinReactiveMockApplication.java
@@ -0,0 +1,13 @@
+package ru.gorbunov.reactive.mock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKotlinReactiveMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKotlinReactiveMockApplication.class, args);
+	}
+
+}
---
+++ spring-kotlin-reactive-mock/src/test/java/ru/gorbunov/reactive/mock/SpringKotlinReactiveMockApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gorbunov.reactive.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKotlinReactiveMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
