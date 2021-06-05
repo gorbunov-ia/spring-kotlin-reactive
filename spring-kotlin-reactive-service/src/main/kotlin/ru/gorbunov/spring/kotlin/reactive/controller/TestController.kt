2da446aa524c527a4b6f5a5ccdafae85cfddd6a5
gorbunov-ia <Grbnv.vn@gmail.com>
2021-06-05 14:51:09 +0300

    Get endpoint was added

--- spring-kotlin-reactive-service/src/main/kotlin/ru/gorbunov/spring/kotlin/reactive/controller/TestController.kt
+++ spring-kotlin-reactive-service/src/main/kotlin/ru/gorbunov/spring/kotlin/reactive/controller/TestController.kt
@@ -1,5 +1,6 @@
 package ru.gorbunov.spring.kotlin.reactive.controller
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,11 +10,19 @@ import java.util.*
 
 @RestController
 @RequestMapping("test")
-class TestController(private val testRepository: TestRepository) {
+class TestController(
+    private val testRepository: TestRepository
+) {
 
     @PostMapping
     suspend fun addTest(): Test {
         return testRepository.save(Test(value = UUID.randomUUID().toString()))
     }
 
+    @GetMapping
+    suspend fun getTest(id: Long): Test? {
+        return testRepository.findById(id)
+    }
+
+    //todo: get info from a remote service
 }
